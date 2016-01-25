d9210401577797cf1913da742b13b02379431ea5
Nursultan Turdaliev <nursultan2010@gmail.com>
2016-01-25 11:00:42 +0100

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name untitledApp
+ * @description
+ * # untitledApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('untitledApp', [
+    'ngAnimate'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name untitledApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the untitledApp
+ */
+angular.module('untitledApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
