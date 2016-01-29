4cc24f488e14d1889d22fd3a56e5afe362d23fd7
Nursultan Turdaliev <nursultan2010@gmail.com>
2016-01-29 23:26:29 +0100

    Dist

--- scripts/data/questions.js
+++ scripts/data/questions.js
@@ -1,15 +1,38 @@
 [
   {
     "id": 1,
-    "title": "Hello, It's me!",
     "code": "(function(){\n   return typeof arguments;\n})();",
     "versions": ["\"object\"", "\"array\"", "\"arguments\"", "\"undefined\""],
-    "answer": "A"
+    "answer": "\"object\""
   },
   {
     "id": 2,
-    "title": "Hello, It's me again!",
-    "versions": ["A", "B", "C", "D"],
-    "answer": "B"
+    "code": "var s = 'Hello';\ntypeof s;",
+    "versions": ["\"string\"", "\"object\"", "\"Object\"", "\"String\""],
+    "answer": "\"string\""
+  },
+  {
+    "id": 3,
+    "code": "typeof (s['toString']())",
+    "versions": ["\"object\"", "\"function\"", "\"undefined\"", "\"string\""],
+    "answer": "\"string\""
+  },
+  {
+    "id": 4,
+    "code": "typeof null",
+    "versions": ["\"null\"", "\"undefined\"", "\"object\"", "\"function\""],
+    "answer": "\"object\""
+  },
+  {
+    "id": 5,
+    "code": "var Object = \"object\";\n typeof Object",
+    "versions": ["\"string\"", "\"object\"", "Object", "function"],
+    "answer": "\"string\""
+  },
+  {
+    "id": 6,
+    "code": "1.1.toString()",
+    "versions": ["\"error\"", "\"1.1\""],
+    "answer": "\"1.1\""
   }
 ]
